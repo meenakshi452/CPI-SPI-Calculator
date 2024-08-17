3ede55fee511cc96f6cebc319c8a68115fc99ec5
meenakshi452 <bethim21@iitk.ac.in>
2024-08-17 14:52:23 +0530

    auth backend done

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from "dotenv"
+dotenv.config();
+
+import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+const app = express();const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server listening on port ${PORT}!!`);
+})
