40d1afb4ba35b985f179866c440a8e37a3253a8b
meenakshi452 <bethim21@iitk.ac.in>
2024-08-24 13:30:34 +0530

    get messages, send messages endpoint done

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
 import express from 'express'
 import dotenv from "dotenv"
-dotenv.config();
+import cookieParser from 'cookie-parser';
+
 
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.route.js"
+import userRoutes from "./routes/user.route.js"
+
 import connectToMongoDB from './db/connectToMongoDB.js';
 
-const app = express();const PORT = process.env.PORT || 5000;
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 
 app.listen(PORT, () => {
