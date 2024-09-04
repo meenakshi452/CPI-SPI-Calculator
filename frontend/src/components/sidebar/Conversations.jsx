99c71ca5f2bfc36a0d5885fcc43a4fc4771807d3
meenakshi452 <bethim21@iitk.ac.in>
2024-09-04 22:28:22 +0530

    frontend connect with backend

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import Conversation from './Conversation'
+import useGetConversations from "../../hooks/useGetConversation";
+import { getRandomEmoji } from "../../uitils/emojis";
+import Conversation from "./Conversation";
 
-export default function Conversations() {
-  return (
-    <div className='py-2 flex flex-col overflow-auto'>
-        <Conversation/>
-    </div>
-  )
-}
+const Conversations = () => {
+	const { loading, conversations } = useGetConversations();
+  
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
+
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
+};
+export default Conversations;
