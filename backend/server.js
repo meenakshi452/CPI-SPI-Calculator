b806e90a7f516a8290e7a9201c26dfc7c6150a0f
meenakshi452 <bethim21@iitk.ac.in>
2024-09-06 13:00:10 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from "dotenv"
+import path from "path"
 import cookieParser from 'cookie-parser';
 
 
@@ -13,6 +14,8 @@ import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname= path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -22,7 +25,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*",(req, res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 server.listen(PORT, () => {
     connectToMongoDB()
     console.log(`Server listening on port ${PORT}!!`);
